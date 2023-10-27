75d8ec6ae40dcb854d82e48abe61c0f35abeca2b
neoy.zhang <neoy.zhang@ibaiqiu.com>
2023-10-27 16:52:30 +0800

    feat：init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href='ssss' className="App-link">
+          react knowledge base sss
+        </a>
+      </header>
+    </div >
+  );
+}
+
+export default App;
